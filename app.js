7dec73d6d5508d79be27806ed520734bb0197685
Utkarsh Kumar <utkarsh9799@gmail.com>
2019-07-20 23:37:23 +0530

    Major updates including vapid keys

--- app.js
+++ app.js
@@ -3,10 +3,20 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var webpush = require('web-push');
+var bodyParser = require('body-parser');
 
 var indexRouter = require('./routes/index');
+var subscribeRouter = require('./routes/subscribe');
 
 var app = express();
+
+app.use(bodyParser.json());
+
+const publicVapidKey = 'BGElE2g3jSv8XpleYspc4A9YX-NGcub-s4pK9uX-zojO3FPpDBzYqaENzOgyMP4dXS-SfQ9Q2ZYh30werqTwnrA';
+const privateVapidKey = '0I_tNYwUSzGFk5MzJSU_5j8XLl16jBiCercA6p9-goI';
+
+webpush.setVapidDetails('mailto:t@t.com',publicVapidKey,privateVapidKey);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,6 +29,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/subscribe', subscribeRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/subscribe.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+var webpush = require('web-push');
+
+router.post('/subscribe', (req,res) => {
+  // Get push subscription object
+  const subscription = req.body;
+
+  // Send 201 - resource created successfully
+  res.status(201).json({});
+
+  // Creating payload
+  const payload = JSON.stringify({ title: 'Push Test' });
+
+  // Passing object into sendNotification
+  webpush.sendNotification(subscription, payload).catch(console.error(err));
+  );
+});
+
+module.exports = router;
